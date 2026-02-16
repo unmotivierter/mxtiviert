11c752a23e71906a5e966ba75d05bcf3b9fbd753
juergen <juergen@copenhagen>
2026-02-16 14:43:50 +0100

    added widgets

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -7,28 +7,46 @@ void main() {
 class Calc extends StatefulWidget {
   const Calc({super.key});
 
+
   @override
   State<Calc> createState() => _CalcState();
 }
 
+enum Operators{
+  add, 
+  subtract,
+  multiply,
+  divide,
+}
+
+
 class _CalcState extends State<Calc> {
+
+  double num1 = 0;
+  double num2 = 0;
+  var oper = Operators.add;
+
+  double addNums(double n1, double n2){
+    return n1 + n2;
+  }
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(child: DisplayBar(to_display: "Halllloooooo")),
+        body: Center(child: DisplayBar(toDisplay: "Halllloooooo")),
       ),
     );
   }
 }
 
 class DisplayBar extends StatelessWidget {
-  const DisplayBar({super.key, required this.to_display});
+  const DisplayBar({super.key, required this.toDisplay});
 
-  final String to_display;
+  final String toDisplay;
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text(to_display));
+    return Container(child: Text(toDisplay));
   }
 }
