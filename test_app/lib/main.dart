9c33fc1bd2703137a18204abe647d3073ee39d88
unmxtiviert <140959348+unmxtiviert@users.noreply.github.com>
2026-02-16 16:34:33 +0100

    Buttonsv1

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -11,18 +11,66 @@ class Calc extends StatefulWidget {
   State<Calc> createState() => _CalcState();
 }
 
-enum Operators { add, subtract, multiply, divide }
+enum Operators { add, subtract, multiply, divide, equal, clear }
 
 class _CalcState extends State<Calc> {
   double num1 = 0;
   double num2 = 0;
+  bool num1Selected = true;
   double res = 0;
-  var oper = Operators.add;
+  var oper;
 
-  void setRes(double n1) {
+  void setRes() {
     setState(() {
-      res = n1;
+      num1Selected = true;
+      switch (oper) {
+        case Operators.add:
+          res = num1 + num2;
+          break;
+        case Operators.subtract:
+          res = num1 - num2;
+          break;
+        case Operators.multiply:
+          res = num1 * num2;
+          break;
+        case Operators.divide:
+          res = num1 / num2;
+          break;
+        default:
+          break;
+      }
     });
+  }
+
+  void setNum(int n1) {
+    setState(() {
+      if (num1Selected) {
+        num1 = n1.toDouble();
+        num1Selected = false;
+      } else {
+        num2 = n1.toDouble();
+      }
+    });
+  }
+
+  void clear() {
+    setState(() {
+      num1 = 0;
+      num2 = 0;
+      res = 0;
+      num1Selected = true;
+      setRes();
+    });
+  }
+
+  void handleOperations(Operators op) {
+    if (op == Operators.equal) {
+      setRes();
+    } else if (op == Operators.clear) {
+      clear();
+    } else {
+      oper = op;
+    }
   }
 
   @override
@@ -32,10 +80,67 @@ class _CalcState extends State<Calc> {
         body: Center(
           child: Column(
             children: [
-              DisplayBar(toDisplay: "$res"),
-              Buttons(updateNums: setRes),
+              DisplayBar(toDisplay: "$num1 and $num2 equals $res"),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    children: [
+                      Buttons(updateNums: setNum, id: 7),
+                      Buttons(updateNums: setNum, id: 4),
+                      Buttons(updateNums: setNum, id: 1),
+                    ],
+                  ),
+
+                  Column(
+                    children: [
+                      Buttons(updateNums: setNum, id: 8),
+                      Buttons(updateNums: setNum, id: 4),
+                      Buttons(updateNums: setNum, id: 2),
+                      Buttons(updateNums: setNum, id: 0),
+                    ],
+                  ),
+
+                  Column(
+                    children: [
+                      Buttons(updateNums: setNum, id: 9),
+                      Buttons(updateNums: setNum, id: 6),
+                      Buttons(updateNums: setNum, id: 3),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      ActionButtons(
+                        updateOp: handleOperations,
+                        op: Operators.add,
+                      ),
+                      ActionButtons(
+                        updateOp: handleOperations,
+                        op: Operators.subtract,
+                      ),
+                      ActionButtons(
+                        updateOp: handleOperations,
+                        op: Operators.multiply,
+                      ),
+                      ActionButtons(
+                        updateOp: handleOperations,
+                        op: Operators.divide,
+                      ),
+                      ActionButtons(
+                        updateOp: handleOperations,
+                        op: Operators.equal,
+                      ),
+                      ActionButtons(
+                        updateOp: handleOperations,
+                        op: Operators.clear,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ],
-          )),
+          ),
+        ),
       ),
     );
   }
@@ -53,12 +158,35 @@ class DisplayBar extends StatelessWidget {
 }
 
 class Buttons extends StatelessWidget {
-  final Function(double) updateNums;
+  final Function(int) updateNums;
+  final int id;
 
-  const Buttons({super.key, required this.updateNums});
+  const Buttons({super.key, required this.updateNums, required this.id});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: (){updateNums(12);}, child: Icon(Icons.numbers));
+    return ElevatedButton(
+      onPressed: () {
+        updateNums(id);
+      },
+      child: Text("$id"),
+    );
+  }
+}
+
+class ActionButtons extends StatelessWidget {
+  final Function(Operators) updateOp;
+  final Operators op;
+
+  const ActionButtons({super.key, required this.updateOp, required this.op});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        updateOp(op);
+      },
+      child: Text("$op"),
+    );
   }
 }
