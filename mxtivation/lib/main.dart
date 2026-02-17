472832cd077a3f2f6de20fcde97b31baaa155d21
unmxtiviert <140959348+unmxtiviert@users.noreply.github.com>
2026-02-17 15:28:43 +0100

    navigation

--- mxtivation/lib/main.dart
+++ mxtivation/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mxtivation/homescreen.dart';
+import 'package:mxtivation/navigation.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,8 +10,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
-    );
+    return const MaterialApp(home: App());
   }
 }
---
+++ mxtivation/lib/navigation.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:mxtivation/homescreen.dart';
+
+enum TabItems { home, group, calendar, settings }
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  TabItems currentTab = TabItems.home;
+  int currentinx = 0;
+  int selectinx = 0;
+
+  void selectTab(int i) {
+    setState(() {
+      currentTab = TabItems.values[i];
+      print(currentTab);
+      selectinx = i;
+      print(selectinx);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: _buildAppBar(context)),
+      body: _buildBody(context),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.group), label: "Groups"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: "Calendar",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
+        currentIndex: selectinx,
+        selectedItemColor: Colors.amber,
+        unselectedItemColor: Colors.black,
+        onTap: selectTab,
+      ),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    switch (currentTab) {
+      case TabItems.home:
+        return Text("Home");
+
+      case TabItems.group:
+        return Text("The group screen");
+
+      case TabItems.calendar:
+        return Text("Hi");
+
+      case TabItems.settings:
+        return Text("Settings");
+    }
+  }
+
+  Widget _buildAppBar(BuildContext context) {
+    String title = "";
+    switch (currentTab) {
+      case TabItems.home:
+        title = "Home";
+        break;
+      case TabItems.group:
+        title = "Groups";
+        break;
+      case TabItems.calendar:
+        title = "Calendar";
+        break;
+      case TabItems.settings:
+        title = "Settings";
+        break;
+    }
+
+    return Text(title);
+  }
+}
