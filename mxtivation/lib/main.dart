bbbee94f7809873cf2d9247792cd9a180988bfec
unmxtiviert <140959348+unmxtiviert@users.noreply.github.com>
2026-02-17 16:12:37 +0100

    temp

--- mxtivation/lib/main.dart
+++ mxtivation/lib/main.dart
@@ -10,7 +10,10 @@ void main() {
 class MainApp extends StatelessWidget {
   MainApp({super.key});
 
-  final List<StreakItem> streakItems = [StreakItem("Streak 1", 12, 30, true), StreakItem("Streak 2", 0, 4, false)];
+  final List<StreakItem> streakItems = [
+    StreakItem("Streak 1", 12, 30, true),
+    StreakItem("Streak 2", 0, 4, false),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -19,13 +22,13 @@ class MainApp extends StatelessWidget {
       theme: FlexThemeData.light(scheme: FlexScheme.flutterDash),
       darkTheme: FlexThemeData.dark(scheme: FlexScheme.flutterDash),
       //themeMode: ThemeMode.system,
-      themeMode: ThemeMode.light,
+      themeMode: ThemeMode.dark,
       home: App(),
     );
   }
 }
 
-class StreakItem{
+class StreakItem {
   String title;
   int streakCount;
   int streakCountPb;
