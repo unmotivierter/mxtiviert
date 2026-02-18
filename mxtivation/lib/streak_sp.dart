01e3c54b90f7702676c9a6b40b93f975fedd44bd
juergen <juergen@copenhagen>
2026-02-18 14:41:30 +0100

    small placeholder changes

--- mxtivation/lib/streak_sp.dart
+++ mxtivation/lib/streak_sp.dart
@@ -8,22 +8,42 @@ class StreakScreenSp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double wHeight = MediaQuery.of(context).size.height/2.5;
+    final double wWidth = MediaQuery.of(context).size.width/2.5;
     return Scaffold(
       appBar: AppBar(
         title: Text(streakItem.title, style: TextStyle(color: Theme.of(context).colorScheme.primaryContainer),),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        spacing: 20,
         children: [
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 20,
             children: [
-              StreakDisplayWidget(streakItem: streakItem, height: 150, width: 150),
-              StreakDisplayWidget(streakItem: streakItem, height: 150, width: 150),
+              StreakDisplayWidget(streakItem: streakItem, height: wHeight, width: wWidth),
+              //Placeholder
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(height: wHeight, width: wWidth, color: Theme.of(context).colorScheme.errorContainer,),
+              ),
             ],
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 20,
             children: [
-              StreakDisplayWidget(streakItem: streakItem, height: 150, width: 150),
-              StreakDisplayWidget(streakItem: streakItem, height: 150, width: 150),
+              //Placeholder
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(height: wHeight, width: wWidth, color: Theme.of(context).colorScheme.secondaryContainer,),
+              ),
+              //Placeholder
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(height: wHeight, width: wWidth, color: Theme.of(context).colorScheme.tertiaryContainer),
+              ),
             ],
           ),
         ],
